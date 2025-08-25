39b389fd337a588f7a2b83e040793a0f75b907ad
Jemina <jesquibelbernal@gmail.com>
2025-08-25 12:56:00 +0200

    Añadiendo main y home

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Padres Sin Caos 🚀</h1>
+      <p>Bienvenido a la app</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a mi app 🚀</h1>
+      <p>Padres sin caos, ahora en versión digital 😎</p>
+    </div>
+  );
+}
